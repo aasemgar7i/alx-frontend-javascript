8aa500024ccd8d3cf2c4f2e6aa3b78b1d92f69d4
aasemgar7i <aasemgar7i@gamil.com>
2024-06-26 02:05:23 +0300

    task#2

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (success) {
+      resolve(object);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
